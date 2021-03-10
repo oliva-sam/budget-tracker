0737e0c53464114a954b858a66fb0648269b4eea
Melody Monarch <oliva.sam.so@gmail.com>
2021-03-09 22:13:41 -0600

    heroku errors again...

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,9 @@ router.post("/api/transaction", ({body}, res) => {
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+    // .catch(err => {
+    //   res.status(404).json(err);
+    // });
 });
 
 router.post("/api/transaction/bulk", ({body}, res) => {
@@ -16,9 +16,9 @@ router.post("/api/transaction/bulk", ({body}, res) => {
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+    // .catch(err => {
+    //   res.status(404).json(err);
+    // });
 });
 
 router.get("/api/transaction", (req, res) => {
